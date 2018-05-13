e37ed197f475e27ea82c2bb5770c8bba5cf587ba
Ramon Henrique <rhpr@cin.ufpe.br>
2018-05-12 21:40:28 -0300

    Add logging messages in proto_http

--- knot/proto_http.py
+++ knot/proto_http.py
@@ -1,4 +1,5 @@
 import requests
+import logging
 
 class ProtoHttp(object):
     def __parseUrl(self, credentials):
@@ -18,17 +19,24 @@ class ProtoHttp(object):
         return ret
 
     def registerDevice(self, credentials, user_data={}):
-        response = requests.post(self.__parseUrl(credentials) + '/devices', json=user_data)
+        url = self.__parseUrl(credentials) + '/devices'
+        logging.info('POST ' + url)
+        logging.info('json -> '+ str(user_data))
+        response = requests.post(url, json=user_data)
         if response.status_code == 201:
             return response.json()
         elif response.status_code == 404:
             raise Exception('Http Error')
 
     def readData(self, credentials, thing_uuid, user_data={}):
-        response = requests.get(self.__parseUrl(credentials) + '/data/' + thing_uuid + self.__queryParameter(user_data),
-                                    headers=self.__authHeaders(credentials), json=user_data)
+        url = self.__parseUrl(credentials) + '/data/' + thing_uuid + self.__queryParameter(user_data)
+        logging.info('GET ' + url)
+        logging.info('json -> '+ str(user_data))
+        response = requests.get(url, headers=self.__authHeaders(credentials), json=user_data)
         return response.json()
 
     def postData(self, credentials, user_data={}):
-        response = requests.post(self.__parseUrl(credentials) + '/data/' + user_data.get('uuid'),
-                                    headers=self.__authHeaders(credentials), json=user_data)
+        url = self.__parseUrl(credentials) + '/data/' + user_data.get('uuid')
+        logging.info('POST ' + url)
+        logging.info('json -> '+ str(user_data))
+        response = requests.post(url, headers=self.__authHeaders(credentials), json=user_data)
