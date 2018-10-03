c2348d1cd3c949e439baf823a59d51035c77259c
Ramon Henrique <rhpr@cin.ufpe.br>
2018-10-03 19:35:41 -0300

    Add missing verification in event flag handler

--- knotpy/handler.py
+++ knotpy/handler.py
@@ -3,10 +3,18 @@ from .evt_flag import *
 def handleEvtFlagError(flags, timeSec, lowerLimit, upperLimit):
 	if FLAG_TIME&flags and not timeSec:
 		raise Exception('eventFlag require time')
+	if not FLAG_TIME&flags and timeSec:
+		raise Exception('You must use FLAG_TIME in eventFlag')
+
 	if FLAG_LOWER&flags and not lowerLimit:
 		raise Exception('eventFlag require lowerLimit')
+	if not FLAG_LOWER&flags and lowerLimit:
+		raise Exception('You must use FLAG_LOWER in eventFlag')
+
 	if FLAG_UPPER&flags and not upperLimit:
 		raise Exception('eventFlag require upperLimit')
+	if not FLAG_UPPER&flags and upperLimit:
+		raise Exception('You must use FLAG_UPPER in eventFlag')
 
 def handleResponseError(res):
 	if isinstance(res, dict):
